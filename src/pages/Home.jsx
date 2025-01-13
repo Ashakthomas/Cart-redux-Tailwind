e59270326a841b9cffc3d9528774109abcd7e335
Ashakthomas <ashakthomas777@gmail.com>
2025-01-13 22:05:54 +0530

    second commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import Header from '../components/Header'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    
+    <>
+    <Header/>
+    <div className='container px-4 mx-auto' style={{paddingTop:'100px'}}>
+      <div className='grid grid-cols-4 gap-4'>
+        <div className='rounded border p-2 shadow'>
+          <img height={'200px'} width={'100%'} src="https://img.freepik.com/free-vector/shopping-supermarket-cart-with-grocery-pictogram_1284-11697.jpg?semt=ais_hybrid" alt="" />
+          <div className='text-center'>
+            <h3 className='text-xl font-bold'>title</h3>
+            <Link to={'/id/view'} className='bg-violet-600 rounded p-1 mt-3 text-white inline-block'>View More...</Link>
+          </div>
+
+        </div>
+
+      </div>
+    </div>
+    </>
   )
 }
 
--- src/pages/View.jsx
+++ src/pages/View.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import Header from '../components/Header'
 
 const View = () => {
   return (
-    <div>View</div>
+    <>
+    <Header/>
+    <div className='flex flex-col mx-5'>
+      <div className='grid grid-cols-2 items-center h-screen'>
+        <img height={'250px'} width={'350px'} src="https://img.freepik.com/free-vector/shopping-supermarket-cart-with-grocery-pictogram_1284-11697.jpg?semt=ais_hybrid" alt="" />
+        <div>
+          <h3 className='font-bold'>PID: id</h3>
+          <h1 className='text-5xl font-bold'>Title</h1>
+          <h4 className='text-2xl text-red-600 font-bold'>$ 250</h4>
+          <h4>Brand :</h4>
+          <h4>Category :</h4>
+          <p>
+            <span className='font-bold'>Description</span>: Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur expedita itaque distinctio nostrum explicabo accusantium. Sed ad hic, possimus similique asperiores minus. Illum ipsum a similique nam nesciunt aspernatur accusamus?
+          </p>
+          <div className='flex justify-between mt-5'>
+            <button className='bg-blue-600 rounded text-white p-2'>Add to whishlist</button>
+            <button className='bg-green-600 rounded text-white p-2'>Add to cart</button>
+
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+    </>
   )
 }
 
